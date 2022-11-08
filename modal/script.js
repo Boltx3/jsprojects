b59df851f2a430c2c00bcae09a46b8361d0a6298
David.Pham <dav.h.pham@gmail.com>
2022-11-08 02:44:15 +0000

    Simple working modal. Remember to check the z-index.

--- modal/script.js
+++ modal/script.js
@@ -1,2 +1,11 @@
-var modal = document.getElementById("openModal");
-var btn = document.getElementById("");
+const openBtn = document.getElementById("openModal");
+const closeBtn = document.getElementById("closeModal");
+const modal = document.getElementById("modal");
+
+openBtn.addEventListener("click", ( ) => {
+    modal.classList.add("open");
+})
+
+closeBtn.addEventListener("click", ( ) => {
+    modal.classList.remove("open");
+})
