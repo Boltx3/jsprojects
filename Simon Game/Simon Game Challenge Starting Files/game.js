54d7c38ec0fd093aadb8ba68fab3b4898fe9b55b
David Pham <dav.h.pham@gmail.com>
2023-02-27 16:20:23 -0600

    added jquery function to track user input

--- Simon Game/Simon Game Challenge Starting Files/game.js
+++ Simon Game/Simon Game Challenge Starting Files/game.js
@@ -1,10 +1,17 @@
 const buttonColours = ["red", "blue", "green", "yellow"];
 const gamePattern = [];
+const userClickedPattern = [];
+
+$('.btn').click(function(event){
+    var userChosenColour = $(this).attr('id');
+    //pushing it userClickedPattern
+     userClickedPattern.push(userChosenColour);
+    console.log(userClickedPattern);
+});
+
 
 function nextSequence ( ) {
     var randomNumber = Math.floor(Math.random() * 4);
     const randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
 }
-
-$("btn#randomChoseColour").css("background-color","green");
