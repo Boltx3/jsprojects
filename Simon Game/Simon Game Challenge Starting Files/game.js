783aad6cc29f3538123da367876711fd24dc5664
Boltx3 <Dav.h.pham@gmail.com>
2023-02-27 21:28:53 -0600

    Working on animations.

--- Simon Game/Simon Game Challenge Starting Files/game.js
+++ Simon Game/Simon Game Challenge Starting Files/game.js
@@ -2,16 +2,22 @@ const buttonColours = ["red", "blue", "green", "yellow"];
 const gamePattern = [];
 const userClickedPattern = [];
 
-$('.btn').click(function(event){
+$('.btn').click(function( ){
     var userChosenColour = $(this).attr('id');
     //pushing it userClickedPattern
      userClickedPattern.push(userChosenColour);
-    console.log(userClickedPattern);
+     //console.log(userClickedPattern)
 });
-
 
 function nextSequence ( ) {
     var randomNumber = Math.floor(Math.random() * 4);
     const randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    
+}
+
+function playSound(name) {
+
 }
