96b0c91a7f8275fd6e210496925028b92004ac74
Boltx3 <Dav.h.pham@gmail.com>
2023-02-27 21:47:04 -0600

    Added animation and sound when clicking buttons

--- Simon Game/Simon Game Challenge Starting Files/game.js
+++ Simon Game/Simon Game Challenge Starting Files/game.js
@@ -13,9 +13,12 @@ function nextSequence ( ) {
     var randomNumber = Math.floor(Math.random() * 4);
     const randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
-
+    //button animation by ID selector
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-    
+
+    //play audio
+    var audio = new Audio('./sounds/' + randomChosenColour + '.mp3');
+    audio.play();
 }
 
 function playSound(name) {
