69849514d865f32f70025af0807817fa6f916799
Boltx3 <Dav.h.pham@gmail.com>
2023-03-01 19:26:35 -0600

    addeded jquery functionality.

--- Simon Game/game.js
+++ Simon Game/game.js
@@ -1,26 +1,45 @@
-const buttonColours = ["red", "blue", "green", "yellow"];
+const buttonColors = ["red", "blue", "green", "yellow"];
 const gamePattern = [];
 const userClickedPattern = [];
 
+
+$("document").on('keypress', nextSequence());
+
 $('.btn').click(function( ){
-    var userChosenColour = $(this).attr('id');
+    var userChosenColor = $(this).attr('id');
     //pushing it userClickedPattern
-     userClickedPattern.push(userChosenColour);
-     //console.log(userClickedPattern)
+     userClickedPattern.push(userChosenColor);
+     playSound(userChosenColor);
+     animatePress(userChosenColor);
 });
 
 function nextSequence ( ) {
     var randomNumber = Math.floor(Math.random() * 4);
-    const randomChosenColour = buttonColours[randomNumber];
-    gamePattern.push(randomChosenColour);
+    const randomChosenColor = buttonColors[randomNumber];
+    var level = 0;
+    gamePattern.push(randomChosenColor);
     //button animation by ID selector
-    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
 
-    //play audio
-    var audio = new Audio('./sounds/' + randomChosenColour + '.mp3');
-    audio.play();
+    //playSound function
+    playSound(randomChosenColor);
+
+    level += 1;
+    $("#level-title").change("level " + level);
 }
 
 function playSound(name) {
+    //play audio
+    var audio = new Audio('./sounds/' + name + '.mp3');
+    audio.play();
+}
 
+function animatePress(currentColor) {
+    var delayMilliseconds = 1000;
+    //adding the class .press 
+    $("#" + currentColor).addClass("pressed");
+    //function to remove the class after 1000 milliseconds
+    setTimeout( function ( ) {
+        $("#" + currentColor).removeClass("pressed");
+    }, delayMilliseconds);
 }
