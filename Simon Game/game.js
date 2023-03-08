59a6f3ebe617dcbfe204b98d3c42f515d1214b0a
David Pham <dav.h.pham@gmail.com>
2023-03-08 15:25:51 -0600

    Finshed Simon Says Game

--- Simon Game/game.js
+++ Simon Game/game.js
@@ -1,9 +1,19 @@
-const buttonColors = ["red", "blue", "green", "yellow"];
-const gamePattern = [];
-const userClickedPattern = [];
+var buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var start = false;
+var level = 0;
 
 
-$("document").on('keypress', nextSequence());
+$(document).keypress(function ()  {
+    //Check if that game has already started
+    if (!start) {
+    //if not start the game and change the title to level + current level
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        start = true;
+    }
+});
 
 $('.btn').click(function( ){
     var userChosenColor = $(this).attr('id');
@@ -11,12 +21,29 @@ $('.btn').click(function( ){
      userClickedPattern.push(userChosenColor);
      playSound(userChosenColor);
      animatePress(userChosenColor);
+     checkAnswer(userClickedPattern.length-1);
 });
 
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        if (userClickedPattern.length === gamePattern.length ){
+            setTimeout( function () { 
+                nextSequence();
+            }, 1000);
+        } 
+        } else {
+            gameOver();
+        }
+    }
+
 function nextSequence ( ) {
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level " + level);
+
     var randomNumber = Math.floor(Math.random() * 4);
     const randomChosenColor = buttonColors[randomNumber];
-    var level = 0;
+
     gamePattern.push(randomChosenColor);
     //button animation by ID selector
     $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
@@ -24,8 +51,6 @@ function nextSequence ( ) {
     //playSound function
     playSound(randomChosenColor);
 
-    level += 1;
-    $("#level-title").change("level " + level);
 }
 
 function playSound(name) {
@@ -35,11 +60,27 @@ function playSound(name) {
 }
 
 function animatePress(currentColor) {
-    var delayMilliseconds = 1000;
+    const delayMilliseconds = 100;
     //adding the class .press 
     $("#" + currentColor).addClass("pressed");
     //function to remove the class after 1000 milliseconds
     setTimeout( function ( ) {
         $("#" + currentColor).removeClass("pressed");
     }, delayMilliseconds);
+}
+
+function gameOver() {
+    playSound("wrong")
+    $("body").addClass("game-over");
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    setTimeout( function ( ) {
+        $("body").removeClass("game-over");
+    }, 200);
+    startOver();
+}
+
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    start = false;
 }
