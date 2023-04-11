34296fe9a9752bb6129ed04864244a71e9c7bda8
Boltx3 <Dav.h.pham@gmail.com>
2023-04-10 20:44:55 -0500

    Simple counter in vanilla js

--- buttoncounter/script.js
+++ buttoncounter/script.js
@@ -1,8 +1,24 @@
-function counter() {
-    //init counter variable to 0
-    const counter = 0;
+let counterDisplay = document.querySelector('.counter-display');
+let counterPlus = document.querySelector('.counter-plus');
+let counterMinus = document.querySelector('.counter-minus');
+let counterRest = document.querySelector('.counter-reset');
 
-    counter++;
+let count = 0;
+
+counterPlus.addEventListener("click", () =>{
+    count++;
+    updateDisplay();
+})
+
+counterMinus.addEventListener("click", () => {
+    count--;
+    updateDisplay();
+})
+
+counterRest.addEventListener("click", () => {
+    count = 0;
+})
+
+function updateDisplay() {
+    counterDisplay.innerHTML = count;
 }
-
-counter();
